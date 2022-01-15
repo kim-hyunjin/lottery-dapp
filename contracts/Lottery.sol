297de2ee1a279aacb9b1cbb055ab446d478fdeb9
kim-hyunjin <hyunjin1612@gmail.com>
2022-01-15 19:00:49 +0900

    Lottery Bet 기능 구현, 테스트

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,15 +2,74 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Lottery {
-  address public owner;
+    struct BetInfo {
+        uint answerBlockNumber;
+        address payable gambler;
+        bytes challenges;
+    }
 
-  constructor() {
-      owner = msg.sender;
-  }
+    address public owner;
 
-  function getSomeValue() public pure returns (uint value) {
-      return 123;
-  }
+    uint constant internal BLOCK_LIMIT = 256;
+    uint constant internal BET_BLOCK_INTERVAL = 3;
+    uint constant internal BET_AMOUNT = 0.005 ether;
+
+    mapping(uint => BetInfo) private _bets;
+    uint private _tail;
+    uint private _head;
+    uint private _pot;
+
+    event BET(uint index, address gambler, uint amount, bytes challenges, uint answerBlockNumber);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getPot() public view returns(uint pot) {
+        return _pot;
+    }
+
+    /**
+     * @dev 베팅을 한다. 유저는 0.005 ETH를 보내야하고, 다음 세번째 블록의 해시값 앞 네 자리 예측 글자를 보낸다.
+     * @param challenges 예측값
+     * @return 함수가 잘 수행되었는지 확인 
+     */
+    function bet(bytes calldata challenges) public payable returns (bool) {
+        require(msg.value == BET_AMOUNT, "Not enough ETH");
+
+        require(pushBet(challenges), "Fail to ad new Bet Info");
+
+        emit BET(_tail - 1, msg.sender, msg.value, challenges, block.number + BET_BLOCK_INTERVAL);
+
+        return true;
+    }
+
+    function getBetInfo(uint index) public view returns (uint answerBlockNumber, address gambler, bytes memory challenges) {
+        BetInfo memory b = _bets[index];
+        answerBlockNumber = b.answerBlockNumber;
+        gambler = b.gambler;
+        challenges = b.challenges;
+    }
+
+    function pushBet(bytes memory challenges) internal returns (bool) {
+        BetInfo memory b;
+        b.gambler = payable(msg.sender);
+        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL;
+        b.challenges = challenges;
+
+        _bets[_tail] = b;
+        _tail++;
+
+        return true;
+    }
+
+    function popBet(uint index) internal returns (bool) {
+        delete _bets[index];
+        return true;
+    }
+
+    // Distribute
+        // check the answer
 
 
 }
--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,18 +1,42 @@
 const Lottery = artifacts.require("Lottery");
+const { shouldThrow, expectEvent } = require("./utils");
 
 // user1, user2, ... 에 ganache의 accounts가 순서대로 들어옴
 contract("Lottery", function ([deployer, user1, user2]) {
     let lottery;
+    const betAmount = 5 * 10 ** 15;
+    const betBlockInterval = 3;
     beforeEach(async () => {
         lottery = await Lottery.new();
     });
 
-    it("Basic test", async () => {
-        let owner = await lottery.owner();
-        let value = await lottery.getSomeValue();
+    it("getPot should return current pod", async () => {
+        let pot = await lottery.getPot();
+        assert.equal(pot, 0);
+    });
 
-        console.log(owner);
-        console.log(value);
-        assert.equal(value, 123);
+    describe("Bet", function () {
+        it("should fail when the bet money is not 0.005 ETH", async () => {
+            await shouldThrow(lottery.bet("0xab", { from: user1, value: 1 }));
+        });
+
+        it("1 bet test", async () => {
+            const challenges = "0xab";
+            const receipt = await lottery.bet(challenges, { from: user1, value: betAmount });
+
+            const pot = await lottery.getPot();
+            assert.equal(pot, 0);
+
+            const contractBalance = await web3.eth.getBalance(lottery.address);
+            assert.equal(contractBalance, betAmount);
+
+            const currentBlockNumber = await web3.eth.getBlockNumber();
+            const bet = await lottery.getBetInfo(0);
+            assert.equal(bet.answerBlockNumber, currentBlockNumber + betBlockInterval);
+            assert.equal(bet.gambler, user1);
+            assert.equal(bet.challenges, challenges);
+
+            await expectEvent(receipt.logs, "BET");
+        });
     });
 });
