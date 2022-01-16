3481041a357d5bd064f6a259ae683e58d1ecdfcb
kim-hyunjin <hyunjin1612@gmail.com>
2022-01-16 19:11:49 +0900

    베팅액 분배 기능 구현, 테스트

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -8,7 +8,8 @@ contract Lottery {
         bytes1 challenges;
     }
 
-    address public owner;
+    address payable public owner;
+    bytes32 public answerForTest;
 
     uint constant internal BLOCK_LIMIT = 256;
     uint constant internal BET_BLOCK_INTERVAL = 3;
@@ -18,6 +19,7 @@ contract Lottery {
     uint private _tail;
     uint private _head;
     uint private _pot;
+    bool private mode = false; // false: test mode, true: real mode
 
     enum BlockStatus {
         Checkable, NotRevealed, BlockLimitPassed
@@ -28,9 +30,27 @@ contract Lottery {
     }
 
     event BET(uint index, address gambler, uint amount, bytes1 challenges, uint answerBlockNumber);
+    event WIN(uint index, address gambler, uint amount, bytes1 challenges, bytes1 answer, uint answerBlockNumber);
+    event FAIL(uint index, address gambler, uint amount, bytes1 challenges, bytes1 answer, uint answerBlockNumber);
+    event DRAW(uint index, address gambler, uint amount, bytes1 challenges, bytes1 answer, uint answerBlockNumber);
+    event REFUND(uint index, address gambler, uint amount, bytes1 challenges, uint answerBlockNumber);
 
     constructor() {
-        owner = msg.sender;
+        owner = payable(msg.sender);
+    }
+
+
+    /**
+     * @dev 베팅과 정답 체크를 한다.
+     * @param challenges 예측값
+     * @return 함수가 잘 수행되었는지 확인 
+     */
+    function betAndDistribute(bytes1 challenges) public payable returns (bool) {
+        bet(challenges);
+
+        distribute();
+
+        return true;
     }
 
     /**
@@ -48,27 +68,62 @@ contract Lottery {
         return true;
     }
 
+    /**
+     * @dev 베팅결과를 확인하고 팟머니를 분배한다.
+     * 한 글자도 못 맞추면 실패: 팟머니에 베팅액 축적.
+     * 두 글자 모두 맞추면 성공: 팟머니 획득.
+     * 한 글자만 맞추었거나 정답을 확인할 수 없는 경우 : 베팅액 돌려줌.
+     */
     function distribute() public {
         uint cur;
+        uint transferAmount;
         BetInfo memory b;
-        BlockStatus currentBlockStatus;
+        BlockStatus status;
+        BettingResult result;
+
         for(cur=_head; cur <= _tail; cur++) {
             b = _bets[cur];
-            currentBlockStatus = getBlockStatus(b.answerBlockNumber);
-            if(currentBlockStatus == BlockStatus.Checkable) {
+            status = getBlockStatus(b.answerBlockNumber);
 
+            if(status == BlockStatus.Checkable) {
+                bytes32 answer = getAnswerBlock(b.answerBlockNumber);
+                result = isMatch(b.challenges, answer);
+                
+                if(result == BettingResult.Win) {
+                    transferAmount = transferAfterPayingFee(b.gambler, _pot + BET_AMOUNT);
+                    _pot = 0;
+
+                    emit WIN(cur, b.gambler, transferAmount, b.challenges, answer[0], b.answerBlockNumber);
+                }
+
+                if(result == BettingResult.Fail) {
+                    _pot += BET_AMOUNT;
+
+                    emit FAIL(cur, b.gambler, 0, b.challenges, answer[0], b.answerBlockNumber);
+                }
+
+                if(result == BettingResult.Draw) {
+                    transferAmount = transferAfterPayingFee(b.gambler, BET_AMOUNT);
+
+                    emit DRAW(cur, b.gambler, transferAmount, b.challenges, answer[0], b.answerBlockNumber);
+                }
             }
 
-            if(currentBlockStatus == BlockStatus.NotRevealed) {
+            if(status == BlockStatus.NotRevealed) {
                 break;
             }
 
-            if(currentBlockStatus == BlockStatus.BlockLimitPassed) {
-                
+            if(status == BlockStatus.BlockLimitPassed) {
+                transferAmount = transferAfterPayingFee(b.gambler, BET_AMOUNT);
+
+                emit REFUND(cur, b.gambler, transferAmount, b.challenges, b.answerBlockNumber);
             }
+ 
+            
             popBet(cur);
         }
 
+        _head = cur;
     }
 
     function getPot() public view returns(uint pot) {
@@ -104,6 +159,12 @@ contract Lottery {
         }
 
         return BettingResult.Fail;
+    }
+
+    function setAnswerForTest(bytes32 answer) public returns(bool) {
+        require(msg.sender == owner, "only owner can set answer for test");
+        answerForTest = answer;
+        return true;
     }
 
     function getFirstWord(bytes1 word) internal pure returns(bytes1) {
@@ -151,6 +212,21 @@ contract Lottery {
         }
 
         return BlockStatus.BlockLimitPassed;
+    }
+
+    function getAnswerBlock(uint answerBlockNumber) internal view returns(bytes32 answer) {
+        return mode ? blockhash(answerBlockNumber) : answerForTest;
+    }
+
+    function transferAfterPayingFee(address payable gambler, uint amount) internal returns (uint) {
+        uint fee = mode ? amount / 100 : 0;
+        uint amountWithoutFee = amount - fee;
+
+        gambler.transfer(amountWithoutFee);
+
+        owner.transfer(fee);
+
+        return amountWithoutFee;
     }
 
 }
