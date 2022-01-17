e2134f178fe74084b645fcd42007d8ced0640e62
kim-hyunjin <hyunjin1612@gmail.com>
2022-01-17 22:01:55 +0900

    event table component 분리

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Web3 from "web3";
 import { lotteryAbi } from "./abi/Lottery";
 import { Contract } from "web3-eth-contract";
+import EventTable from "./components/EventTable";
 
 declare global {
     interface Window {
@@ -11,47 +12,27 @@ declare global {
     }
 }
 
-type EventRecord = {
-    index: string;
-    gambler?: string;
-    challenges?: string;
-    answer?: string;
-    betBlockNumber?: string;
-    targetBlockNumber?: string;
-    amount?: string;
-    status: "WIN" | "FAIL" | "DRAW" | "NotRevealed";
+type AppState = {
+    pot: string;
+    challenges: [string, string];
 };
 
 const lotteryAddress = "0xffD4c5BfB668874534d12f9D9bE0A98CdA59FdcD";
-class App extends React.Component<
-    any,
-    {
-        betRecords: EventRecord[];
-        winRecords: EventRecord[];
-        failRecords: EventRecord[];
-        drawRecords: EventRecord[];
-        finalRecords: EventRecord[];
-        pot: string;
-        challenges: [string, string];
-    }
-> {
+
+class App extends React.Component<any, AppState> {
     web3!: Web3;
     account!: string;
     lotteryContract!: Contract;
     firstWordInput: RefObject<HTMLInputElement>;
     secondWordInput: RefObject<HTMLInputElement>;
+    interval?: NodeJS.Timer;
 
     constructor(props: any) {
         super(props);
 
         this.state = {
-            betRecords: [],
-            winRecords: [],
-            failRecords: [],
-            drawRecords: [],
             pot: "0",
             challenges: ["", ""],
-            finalRecords: [],
         };
         this.firstWordInput = React.createRef<HTMLInputElement>();
         this.secondWordInput = React.createRef<HTMLInputElement>();
@@ -60,19 +41,14 @@ class App extends React.Component<
     async componentDidMount() {
         await this.initWeb3();
 
-        this.pollData();
-        setInterval(this.pollData, 10000);
+        this.getPot();
+        setInterval(this.getPot, 3000);
         this.firstWordInput.current?.focus();
     }
 
-    pollData = async () => {
-        await this.getPot();
-        await this.getBetEvents();
-        await this.getWinEvents();
-        await this.getFailEvents();
-        await this.getDrawEvents();
-        this.makeFinalRecords();
-    };
+    componentWillUnmount() {
+        if (this.interval) clearInterval(this.interval);
+    }
 
     initWeb3 = async () => {
         this.web3 = new Web3(window.ethereum);
@@ -106,110 +82,6 @@ class App extends React.Component<
         // 스마트 컨트랙트의 상태를 변경하지 않고, 값만 읽어올 때 call() 호출
         const pot = await this.lotteryContract.methods.getPot().call();
         this.setState({ pot: this.web3.utils.fromWei(pot) });
-    };
-
-    makeFinalRecords = () => {
-        let f = 0,
-            w = 0,
-            d = 0;
-        const records = [...this.state.betRecords];
-        for (const record of records) {
-            if (
-                this.state.winRecords.length > 0 &&
-                record.index === this.state.winRecords[w].index
-            ) {
-                record.status = this.state.winRecords[w].status;
-                record.answer = this.state.winRecords[w].answer;
-                record.amount = this.web3.utils.fromWei(
-                    String(this.state.winRecords[w].amount),
-                    "ether"
-                );
-                if (w < this.state.winRecords.length - 1) w++;
-            } else if (
-                this.state.failRecords.length > 0 &&
-                record.index === this.state.failRecords[f].index
-            ) {
-                record.status = this.state.failRecords[f].status;
-                record.answer = this.state.failRecords[f].answer;
-                if (f < this.state.failRecords.length - 1) f++;
-            } else if (
-                this.state.drawRecords.length > 0 &&
-                record.index === this.state.drawRecords[d].index
-            ) {
-                record.status = this.state.drawRecords[d].status;
-                record.answer = this.state.drawRecords[d].answer;
-                if (d < this.state.drawRecords.length - 1) d++;
-            }
-        }
-        this.setState({ finalRecords: records });
-    };
-
-    getBetEvents = async () => {
-        const events = await this.lotteryContract.getPastEvents("BET", {
-            fromBlock: 0,
-            toBlock: "latest",
-        });
-        const records: EventRecord[] = events.reverse().map((e) => {
-            return {
-                index: parseInt(e.returnValues.index, 10).toString(),
-                gambler: e.returnValues.gambler,
-                betBlockNumber: String(e.blockNumber),
-                targetBlockNumber: e.returnValues.answerBlockNumber.toString(),
-                challenges: e.returnValues.challenges,
-                status: "NotRevealed",
-            };
-        });
-        console.log("BET", records);
-        this.setState({ betRecords: records });
-    };
-
-    getWinEvents = async () => {
-        const events = await this.lotteryContract.getPastEvents("WIN", {
-            fromBlock: 0,
-            toBlock: "latest",
-        });
-        const records: EventRecord[] = events.reverse().map((e) => {
-            return {
-                index: parseInt(e.returnValues.index, 10).toString(),
-                answer: e.returnValues.answer,
-                amount: String(e.returnValues.amount),
-                status: "WIN",
-            };
-        });
-        console.log("WIN", records);
-        this.setState({ winRecords: records });
-    };
-
-    getFailEvents = async () => {
-        const events = await this.lotteryContract.getPastEvents("FAIL", {
-            fromBlock: 0,
-            toBlock: "latest",
-        });
-        const records: EventRecord[] = events.reverse().map((e) => {
-            return {
-                index: parseInt(e.returnValues.index, 10).toString(),
-                answer: e.returnValues.answer,
-                status: "FAIL",
-            };
-        });
-        console.log("FAIL", records);
-        this.setState({ failRecords: records });
-    };
-
-    getDrawEvents = async () => {
-        const events = await this.lotteryContract.getPastEvents("DRAW", {
-            fromBlock: 0,
-            toBlock: "latest",
-        });
-        const records: EventRecord[] = events.reverse().map((e) => {
-            return {
-                index: parseInt(e.returnValues.index, 10).toString(),
-                answer: e.returnValues.answer,
-                status: "DRAW",
-            };
-        });
-        console.log("DRAW", records);
-        this.setState({ drawRecords: records });
     };
 
     handleInputChange = (position: "first" | "second", value: string) => {
@@ -281,36 +153,7 @@ class App extends React.Component<
                         베팅
                     </button>
                 </section>
-                <section className="container text-center mt-5">
-                    <table className="table table-dark table-striped">
-                        <thead>
-                            <tr>
-                                <th>Index</th>
-                                <th>Address</th>
-                                <th>Challenges</th>
-                                <th>Answer</th>
-                                <th>Pot</th>
-                                <th>Status</th>
-                                <th>AnswerBlockNumber</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.finalRecords.map((record, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{record.index}</td>
-                                        <td>{record.gambler}</td>
-                                        <td>{record.challenges}</td>
-                                        <td>{record.answer}</td>
-                                        <td>{record.amount}</td>
-                                        <td>{record.status}</td>
-                                        <td>{record.targetBlockNumber}</td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </section>
+                <EventTable web3={this.web3} lotteryContract={this.lotteryContract} />
             </main>
         );
     }
