6d47842ea6593a49fd2ef7430e2450fba7c0b012
kim-hyunjin <hyunjin1612@gmail.com>
2022-01-17 14:42:46 +0900

    webpack config customize

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Web3 from "web3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+declare global {
+    interface Window {
+        web3: any;
+        ethereum: any;
+    }
+}
+
+class App extends React.Component {
+    web3!: Web3;
+
+    async componentDidMount() {
+        await this.initWeb3();
+    }
+
+    async initWeb3() {
+        if (window.ethereum) {
+            this.web3 = new Web3(window.ethereum);
+            console.log(this.web3);
+        } else if (window.web3) {
+            this.web3 = new Web3(window.web3.currentProvider);
+        }
+    }
+
+    render(): React.ReactNode {
+        return (
+            <div>
+                <h1>HELLO!</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
